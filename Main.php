96c5731ceac1c41a9d788d478d323b979bce5288
Marcus Povey <marcus@instruct-eric.eu>
2020-07-31 12:22:03 +0100

    Working review wrapper

--- Main.php
+++ Main.php
@@ -2,6 +2,8 @@
 
 namespace IdnoPlugins\ReviewLogin;
 
+use Idno\Core\Idno;
+
 class Main extends \Idno\Common\Plugin {
     
     private $userinfo_url = 'https://staging.reviewtalentfeedback.com/api/v1/users/me'; // TODO: change to live value
@@ -24,25 +26,35 @@ class Main extends \Idno\Common\Plugin {
                 
                 $access_token = $data['access_token'];
                 
+                if (empty($access_token)) {
+                    throw new \RuntimeException('Missing access token');
+                }
+                
                 Idno::site()->logging()->debug('Review Login Token: ' . $access_token);
                 
                 $endpoint = \Idno\Core\site()->config()->config['review_login_userinfo_url'] ?? $this->userinfo_url;
                 
-                $userdata = \Idno\Core\Webservice::get($endpoint, [], [
-                    'Authorization: Bearer ' . $access_token
-                ]);
+                /*$userdata = \Idno\Core\Webservice::get($endpoint, [
+                ], [
+                    'Authorization: Bearer ' . $access_token,
+                    'Cache-Control: no-cache'
+                ]);*/
+                $userdata = file_get_contents($endpoint . '?api_key=' . urlencode($access_token)); // Because doorkeeper doesn't like _something_ about the webservices call
                 
                 Idno::site()->logging()->debug('Review response: ' . var_export($userdata, true));
                 
-                if (!empty($userdata['content'])) {
-                    $userdata = json_decode($userdata['content'], true);
-                    if (!empty($userdata['_id'])) {
+                if (!empty($userdata)) {
+                    $userdata = json_decode($userdata, true);
+                    if (!empty($userdata['error'])) {
+                        throw new \RuntimeException($userdata['error']);
+                    }
+                    if (!empty($userdata['id'])) {
                         
                         $user = \IdnoPlugins\OAuth2Client\Main::getUser(
                                 $data['context'], 
-                                $userdata['_id']['$oid'], 
+                                $userdata['id'], 
                                 $userdata['email'], 
-                                $userdata['user_name'], 
+                                $userdata['name'], 
                                 $userdata['email']);
                         
                         if (!empty($user)) {
